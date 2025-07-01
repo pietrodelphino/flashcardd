d6e025a93d4497824c290a855646c5b3fa8cfc01
Pietro Matheus Delphino Garcia <garcia.pietro@escola.pr.gov.br>
2025-07-01 08:44:34 -0300

    primeira versão do meu código

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML, ou Linguagem de Marcação de Hipertexto, é a linguagem padrão utilizada para criar e estruturar páginas web.la define a estrutura do conteúdo, como textos, imagens, links e outros elementos, organizando-os de forma lógica para que os navegadores possam interpretá-los e exibi-los corretamente. '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a estrutura básica de um documento HTML?',
+    'A estrutura básica de um documento HTML consiste em: <!DOCTYPE html>,<html>,<head>,<body>,<!DOCTYPE html>'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'Como criar um formulário no HTML??',
+    'Para criar um formulário HTML, utilize a tag <form> para definir o formulário e seus atributos como method e action, que especificam como os dados serão enviados.Dentro do formulário, utilize tags como <input>, <textarea>, <select>, entre outras, para criar os campos do formulário. Cada campo deve ter um name e, opcionalmente, um id.Use a tag <label> para associar um texto descritivo a cada campo, e a tag <button> para criar o botão de envio. '
